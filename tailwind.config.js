b70dfc7ef8e4a251f242b85d3f2371acd8803eb5
borislavpachev <borislav.pachev@gmail.com>
2024-11-08 16:27:22 +0200

    style: Reusable styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pt-sans-regular': ['pt-sans-caption-regular', 'sans-serif'],
+        'pt-sans-bold': ['pt-sans-caption-bold', 'sans-serif'],
+        chicle: ['Chicle', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-sm': '0px 3px 6px #00000012',
+        'custom-md': '0px 10px 20px #00000017',
+      },
+      colors: {
+        'main-tx-color': '#333333',
+        'secondary-tx-color': '#362E29',
+        'custom-green': '#23B21F',
+      },
+    },
   },
   plugins: [],
 };
