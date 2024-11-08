ba140a0341ff136d2ca6c54116bb1964f23e8af2
borislavpachev <borislav.pachev@gmail.com>
2024-11-08 20:12:22 +0200

    refactor: Better mapping of footer icons in use

---
+++ src/utils/constants.js
@@ -0,0 +1,25 @@
+export const paymentIcons = [
+  { name: 'Master Card logo', src: '/img/icons/mastercard.svg' },
+  { name: 'Maestro logo', src: '/img/icons/maestro.svg' },
+  { name: 'VISA logo', src: '/img/icons/visa.svg' },
+  { name: 'Diners club logo', src: '/img/icons/diners-club.svg' },
+  { name: 'Verified by Visa logo', src: '/img/icons/verified-by-visa.svg' },
+  {
+    name: 'Master Card Secure Code logo',
+    src: '/img/icons/mastercard-secure-code.svg',
+  },
+  { name: 'PayPal logo', src: '/img/icons/paypal.svg' },
+  { name: 'NORTON by Symantec logo', src: '/img/icons/norton-by-symantec.svg' },
+];
+
+export const deliveryIcons = [
+  { name: 'DHL logo', src: '/img/icons/DHL-logo.svg' },
+  { name: 'BRT logo', src: '/img/icons/BRT-logo.svg' },
+  { name: 'Speedy logo', src: '/img/icons/SPEEDY-logo.svg' },
+];
+
+export const socialMediaIcons = [
+  { name: 'Facebook logo', src: '/img/icons/facebook.svg' },
+  { name: 'Instagram logo', src: '/img/icons/instagram.svg' },
+  { name: 'Google-plus logo', src: '/img/icons/google-plus.svg' },
+];
