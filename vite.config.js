78a2539b89b41a4851a5ea3ce4206314df884e0d
borislavpachev <borislav.pachev@gmail.com>
2024-11-08 16:24:54 +0200

    feat: Absolute imports in use

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: { alias: [{ find: '@', replacement: resolve('src') }] },
+});
