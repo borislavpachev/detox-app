8e8a46f30b1e5cdaf5068fa47b0f1ff85f77a0ca
borislavpachev <borislav.pachev@gmail.com>
2024-11-09 23:31:29 +0200

    feat: Better icons exporting

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,1 +1,4 @@
 export { default as LogoIcon } from './LogoIcon';
+export { default as TruckDeliveryIcon } from './TruckDeliveryIcon';
+export { default as MoneyIcon } from './MoneyIcon';
+export { default as TestPeriodIcon } from './TestPeriodIcon';
