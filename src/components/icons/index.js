bad68f669a3bf6503c2a3c3356e2462a2739925f
borislavpachev <borislav.pachev@gmail.com>
2024-11-10 16:52:49 +0200

    feat: About section icons created

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,3 +2,7 @@ export { default as LogoIcon } from './LogoIcon';
 export { default as TruckDeliveryIcon } from './TruckDeliveryIcon';
 export { default as MoneyIcon } from './MoneyIcon';
 export { default as TestPeriodIcon } from './TestPeriodIcon';
+export { default as BowlIcon } from './BowlIcon';
+export { default as BodyClockIcon } from './BodyClockIcon';
+export { default as DetoxIcon } from './DetoxIcon';
+
