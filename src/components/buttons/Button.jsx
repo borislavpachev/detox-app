e6849259a8b18237a8e7334cb3b093ff2b944589
borislavpachev <borislav.pachev@gmail.com>
2024-11-10 20:24:33 +0200

    style: Clicking effect when button is active

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types';
 
 export default function Button({ children }) {
-  return <button className='rounded-[27px] w-[179px] h-[40px] font-pt-sans-bold text-custom-green shadow-custom-md border-2 border-custom-green uppercase
-  transition-colors ease-in duration-300  hover:border-transparent hover:bg-custom-green hover:text-white'>{children}</button>;
+  return (
+    <button
+      className="rounded-[27px] px-10 py-2 font-pt-sans-bold text-custom-green shadow-custom-md border-2 border-custom-green uppercase
+  transition-colors ease-in duration-300 hover:border-transparent hover:bg-custom-green hover:text-white
+   active:shadow-custom-inner active:shadow-black/30"
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
