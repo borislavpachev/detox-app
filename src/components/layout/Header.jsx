5f6cc15563ae6d3e97c85e37522d58dd0eb6639b
borislavpachev <borislav.pachev@gmail.com>
2024-11-10 20:22:40 +0200

    feat: navLinks moved in constant and used with custom styles

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,27 @@
 import { Button } from '../buttons';
 import { LogoIcon } from '../icons';
 
+const navigationLinks = ['За чая', 'Програми', 'Блог', 'BMI Калкулатор'];
+
 export default function Header() {
   return (
-    <header className="flex md:sticky top-0 bg-white z-20 flex-col md:flex-row py-5 md:py-0 h-auto md:h-[74px] mb-[31px] items-center justify-around shadow-custom-sm">
+    <header className="flex flex-col md:flex-row py-5 md:py-0 h-auto md:h-[74px] mb-[31px] items-center justify-around shadow-custom-sm">
       <LogoIcon />
-      <nav className="flex flex-col md:flex-row w-full md:w-[571.66px] md:h-[40px] items-center font-pt-sans-bold text-secondary-tx-color opacity-[1] cursor-default">
+      <nav
+        className="flex flex-col md:flex-row w-full md:w-[571.66px] md:h-[40px] items-center 
+      leading-[20px] font-pt-sans-bold text-secondary-tx-color cursor-default"
+      >
         <ul className="flex flex-col md:flex-row w-full md:w-[393px] items-center my-5 md:my-0 gap-5">
-          <li>За чая</li>
-          <li>Програми</li>
-          <li>Блог</li>
-          <li>BMI Калкулатор</li>
+          {navigationLinks.map((link) => {
+            return (
+              <li
+                key={link}
+                className="hover:scale-110 hover:text-black transition ease-in-out duration-300 underline-offset-8 cursor-pointer hover:underline"
+              >
+                {link}
+              </li>
+            );
+          })}
         </ul>
         <Button>купи сега</Button>
       </nav>
