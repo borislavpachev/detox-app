d209f9b20ca27f236de0b3211537141f96d773fd
borislavpachev <borislav.pachev@gmail.com>
2024-11-12 18:45:43 +0200

    feat: Cards moving animation added

--- src/pages/HomePage/components/WhyDetox.jsx
+++ src/pages/HomePage/components/WhyDetox.jsx
@@ -1,6 +1,10 @@
 import { ScrollCard } from '@/components';
 import { Section } from '@/components/layout';
 import { whyDetoxIcons } from '@/utils/constants';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { horizontalLoop } from '@/utils/horizontalLoop';
 
 const texts = [
   <span key={1}>
@@ -17,17 +21,45 @@ const texts = [
   </span>,
 ];
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function WhyDetox() {
+  const boxesRef = useRef(null);
+
+  useEffect(() => {
+    const boxes = boxesRef.current;
+    const trigger = ScrollTrigger.create({
+      trigger: boxes,
+      start: 'top 50%',
+      end: 'bottom',
+      onEnter: () => {
+        horizontalLoop('.scroll-card', {
+          repeat: -1,
+          paddingRight: 20,
+          speed: 0.8,
+        });
+      },
+    });
+
+    return () => {
+      trigger.kill();
+    };
+  }, []);
+
   return (
     <Section row={false} className="font-pt-sans-regular text-center">
       <div
         className="flex flex-col text-2xl md:mb-[63px] md:text-[60px]
-      md:leading-[78px] md:mx-[300px] "
+      md:leading-[78px] md:mx-[300px]"
       >
         <p>Причини да консумираш</p>
         <p className="font-pt-sans-bold">Detox</p>
       </div>
-      <div className="flex transform-boxes py-4 w-[95%] overflow-auto gap-5 m-auto">
+      <div
+        ref={boxesRef}
+        style={{ overflowX: 'hidden' }}
+        className="flex items-center justify-center transform-boxes py-6 gap-5 mx-2 my-4"
+      >
         {whyDetoxIcons.map((icon, index) => {
           return <ScrollCard key={icon.alt} data={icon} text={texts[index]} />;
         })}
